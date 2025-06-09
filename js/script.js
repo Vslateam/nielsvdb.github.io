1a2563a3662cc2decc93a10b3f909d31ae99a9e6
Vslateam-NVdB <vdbrandeniels@gmail.com>
2025-06-09 19:25:55 +0200

    ajout du js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-// JavaScript optionnel pour le menu ou animations
+// Injecter le header
+fetch('header.html')
+  .then(res => res.text())
+  .then(data => {
+    document.body.insertAdjacentHTML('afterbegin', data);
+  });
+
+// Injecter le footer
+fetch('footer.html')
+  .then(res => res.text())
+  .then(data => {
+    document.body.insertAdjacentHTML('beforeend', data);
+  });
